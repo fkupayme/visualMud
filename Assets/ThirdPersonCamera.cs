44d2ca7a50a0f47b881fa877fc751e41db783af8
fkupayme <sublimemm+fkupayme@gmail.com>
2013-09-29 16:32:41 -0400

    no message

---
+++ Assets/ThirdPersonCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+	
+	public Vector3 positionOffset = new Vector3(0,10,-10);
+	
+	// Use this for initialization
+	void Start () {
+		positionOffset = transform.position - player.transform.position;
+	}
+	
+	public Transform player;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = player.transform.position + positionOffset;
+		//transform.LookAt(player.transform.position);
+	}
+}
